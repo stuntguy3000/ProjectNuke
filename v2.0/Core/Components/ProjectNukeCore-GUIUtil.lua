efbbfbcdbef367f5b09a13df84ebff295af11dbc
Luke Anderson <stuntguy3000@gmail.com>
2020-03-08 20:25:02 +1030

    Push

--- v2.0/Core/Components/ProjectNukeCore-GUIUtil.lua
+++ v2.0/Core/Components/ProjectNukeCore-GUIUtil.lua
@@ -14,12 +14,12 @@
 function DrawBaseGUI(Title, SubHeading)
   -- Draw Title/Subheading Backgrounds
   paintutils.drawFilledBox(1, 1, 51, 3, colors.yellow)
-	paintutils.drawFilledBox(1, 4, 51, 6, colors.red)
-	
+  paintutils.drawFilledBox(1, 4, 51, 6, colors.red)
+  
   -- Draw Black Checker Pattern
-	DrawBlackSquares(2, 1)
-	DrawBlackSquares(1, 2)
-	DrawBlackSquares(0, 3)
+  DrawBlackSquares(2, 1)
+  DrawBlackSquares(1, 2)
+  DrawBlackSquares(0, 3)
 end
 
 local function DrawBlackSquares(xStart, y) 
@@ -37,25 +37,25 @@ function DrawCenteredText(text, yVal)
 end
 
 function DrawStatus(StatusText)
-	term.setTextColor(colors.gray)
-	DrawCenteredText("                                                                                                                  ", 19)
-	DrawCenteredText(message, 19)
+  term.setTextColor(colors.gray)
+  DrawCenteredText("                                                                                                                  ", 19)
+  DrawCenteredText(message, 19)
 end
 
 -- Used to draw a error messages
 -- Message is assumed to be a table
 --  Table: yValue messageText
 function DrawSuccessMessages(messageLines, timeout)
-	paintutils.drawFilledBox(1, 7, 51, 19, colors.green)
+  paintutils.drawFilledBox(1, 7, 51, 19, colors.green)
   
-	term.setBackgroundColor(colors.green)
-	term.setTextColor(colors.black)
+  term.setBackgroundColor(colors.green)
+  term.setTextColor(colors.black)
   
   for yValue, message in pairs(messageLines) do
     DrawCenteredText(message, yValue)
   end
   
-	DrawStatus("")
+  DrawStatus("")
   
   sleep(timeout)
 end
@@ -64,16 +64,16 @@ end
 -- Message is assumed to be a table
 --  Table: yValue messageText
 function DrawErrorMessages(lines, timeout)
-	paintutils.drawFilledBox(1, 7, 51, 19, colors.red)
+  paintutils.drawFilledBox(1, 7, 51, 19, colors.red)
   
-	term.setBackgroundColor(colors.red)
-	term.setTextColor(colors.black)
+  term.setBackgroundColor(colors.red)
+  term.setTextColor(colors.black)
   
   for yValue, message in pairs(messageLines) do
     DrawCenteredText(message, yValue)
   end
   
-	DrawStatus("")
+  DrawStatus("")
   
   sleep(timeout)
 end
