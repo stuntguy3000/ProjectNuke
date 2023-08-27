191eb1338f208e0ae6152ed5d0fb13e63f9e1d09
Luke Anderson <stuntguy3000@gmail.com>
2023-08-27 10:20:23 +0930

    Add launch parameters to startup file

--- v2.0/Core/ProjectNukeLauncher.lua
+++ v2.0/Core/ProjectNukeLauncher.lua
@@ -10,12 +10,24 @@
   Author: stuntguy3000
 
 --]]
+
+NODOWNLOAD = true
+LOADONLY = false
+
 function DownloadAndExecuteCore()
   print(" > Downloading core...")
   
-  fs.delete("/ProjectNuke/Core/ProjectNukeCore.lua")
-  shell.run("wget https://raw.githubusercontent.com/stuntguy3000/ProjectNuke/master/v2.0/Core/ProjectNukeCore.lua /ProjectNuke/Core/ProjectNukeCore.lua")
-  shell.run("/ProjectNuke/Core/ProjectNukeCore.lua")
+  if (NODOWNLOAD) then
+    shell.run("/ProjectNuke/Core/ProjectNukeCore.lua NODOWNLOAD")
+  elseif (LOADONLY) then
+    fs.delete("/ProjectNuke/Core/ProjectNukeCore.lua")
+    shell.run("wget https://raw.githubusercontent.com/stuntguy3000/ProjectNuke/master/v2.0/Core/ProjectNukeCore.lua /ProjectNuke/Core/ProjectNukeCore.lua")
+    shell.run("/ProjectNuke/Core/ProjectNukeCore.lua LOADONLY")
+  else
+    fs.delete("/ProjectNuke/Core/ProjectNukeCore.lua")
+    shell.run("wget https://raw.githubusercontent.com/stuntguy3000/ProjectNuke/master/v2.0/Core/ProjectNukeCore.lua /ProjectNuke/Core/ProjectNukeCore.lua")
+    shell.run("/ProjectNuke/Core/ProjectNukeCore.lua")
+  end
 end
 
 DownloadAndExecuteCore()
