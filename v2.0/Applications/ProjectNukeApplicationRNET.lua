df2cf2c76d77d7ae857b00e81df9c8c53fdbfac1
Luke Anderson <stuntguy3000@gmail.com>
2023-08-28 21:47:04 +0930

    Begin building out REDNET Monitor App GUI

--- v2.0/Applications/ProjectNukeApplicationRNET.lua
+++ v2.0/Applications/ProjectNukeApplicationRNET.lua
@@ -13,27 +13,53 @@
 
 --]]
 function run()
-  ProjectNukeCoreGUIUtil.tryAttachMonitor()
+  ProjectNukeCoreGUIUtil.DrawBaseGUI(getDisplayName(), nil)
 
-  print("========== REDNET Monitor v0.1 ==========")
-  print("Incoming messages will be printed below.")
-  print("========================================")
-  print("")
+  -- Draw GUI Elements
+  window = ProjectNukeCoreGUIUtil.MainWindow
+  windowSize = {window.getSize()}
 
+  ProjectNukeCoreGUIUtil.DrawFilledBoxInWindow(colors.gray, 2, 9, windowSize[1] - 1, 18, window)
+
+  -- Draw Labels
+  window.setCursorPos(2, 8)
+  window.setBackgroundColour(colors.lightGray)
+  window.setTextColour(colors.gray)
+  window.write("Last Recieved Message:")
+
+  -- Await Message
   while true do
-    -- Custom Copypasta from REDNET Handler
-    senderId, message, protocol = rednet.receive(ProjectNukeCoreRednetHandler.REDNET_PROTOCOL_ID)
-
-    -- Attempt to decrypt the message
-    decryptedMessage = ProjectNukeCoreEncryptionUtil.decrypt(ProjectNukeCoreConfigurationHandler.LoadedConfiguration.encryptionKey, message)
-
-    if (decryptedMessage == nil) then
-      print("Recieved message, but unable to decrypt. Ignoring...")
-    else
-      print("------------------------------")
-      print(decryptedMessage)
-    end
+    printMessage(awaitMessage())
   end
+end
+
+function printMessage(message)
+  -- Reset
+  ProjectNukeCoreGUIUtil.DrawFilledBoxInWindow(colors.gray, 2, 9, windowSize[1] - 1, 18, window)
+  window.setBackgroundColour(colors.gray)
+  window.setTextColour(colors.black)
+
+  -- Sanitize Message
+  message = string.gsub(message, "n", "")
+
+  -- Print
+  window.setCursorPos(2, 3)
+  print(message)
+end
+
+function awaitMessage()
+  -- Custom Copypasta from REDNET Handler
+  senderId, message, protocol = rednet.receive(ProjectNukeCoreRednetHandler.REDNET_PROTOCOL_ID) 
+
+  -- Attempt to decrypt the message
+  decryptedMessage = ProjectNukeCoreEncryptionUtil.decrypt(ProjectNukeCoreConfigurationHandler.LoadedConfiguration.encryptionKey, message)
+
+  if (decryptedMessage ~= nil) then
+    return decryptedMessage
+  end
+
+  -- On error, loop.
+  awaitMessage()
 end
 
 function getDisplayName()
